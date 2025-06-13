c79506c6d999def984fff3faf4f599568d9745c5
Celine SHOGA <c.shoga@alustudent.com>
2025-06-13 17:13:34 +0200

    defining database structure

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,10 +1,12 @@
+PRAGMA foreign_keys = ON;
+
 -- Table for transaction types
 CREATE TABLE IF NOT EXISTS transaction_types (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT UNIQUE NOT NULL
 );
 
--- Table for agents (for withdrawals, etc.)
+-- Table for agents 
 CREATE TABLE IF NOT EXISTS agents (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
